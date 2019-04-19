2d082782451f60d273b66ba143969644ba3e58b6
Aishvarya-Varadharajan <49087166+Aishvarya-Varadharajan@users.noreply.github.com>
2019-04-19 14:00:40 +0530

    Create swift.c

---
+++ swift.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+  int N,K;
+  scanf("%d %d",&N.&K);
+  int arr[N],i;
+  for(i=0;i<N;i++)
+  {
+    scanf("%d",&arr[i]);
+  }
+  for(i=0;i<N;i++)
+  {
+    a[i]=arr[i] << k;
+  }
+   for(i=0;i<N;i++)
+  {
+    printf("%d",a[i]);
+  }
+  }
