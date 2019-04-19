d97a26f5623589cdff1dd677d0686b1fc7b3eb42
Aishvarya-Varadharajan <49087166+Aishvarya-Varadharajan@users.noreply.github.com>
2019-04-19 14:18:11 +0530

    Update swift.c

--- swift.c
+++ swift.c
@@ -2,18 +2,19 @@
 int main()
 {
   int N,K;
-  scanf("%d %d",&N.&K);
-  int arr[N],i;
+  scanf("%d %d",&N,&K);
+  int arr[N],i,a[N];
   for(i=0;i<N;i++)
   {
     scanf("%d",&arr[i]);
   }
   for(i=0;i<N;i++)
   {
-    a[i]=arr[i] << k;
+    a[i]=(arr[i] << 2) | (arr[i] >> ( 8-2));
   }
    for(i=0;i<N;i++)
   {
     printf("%d",a[i]);
   }
+  return 0;
   }
