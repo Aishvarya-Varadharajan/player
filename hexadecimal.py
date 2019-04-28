2bdf31a80347dc3b6e1e47c7fe11e4a5d2e113cd
Aishvarya-Varadharajan <49087166+Aishvarya-Varadharajan@users.noreply.github.com>
2019-04-28 13:58:59 +0530

    Create hexadecimal.py

---
+++ hexadecimal.py
@@ -0,0 +1,11 @@
+num=input()
+length=len(num)
+index=1
+count = 0
+for index in num:
+  if((index >= 'A') and (index <= 'F') or (index.isdigit()) ):
+    count=count+1;
+if count == count:
+  print("yes")
+else
+  print("No")
