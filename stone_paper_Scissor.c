7e1022843f97f875b81b7943905d651c0c8c240a
Aishvarya-Varadharajan <49087166+Aishvarya-Varadharajan@users.noreply.github.com>
2019-04-28 18:43:14 +0530

    Update stone_paper_Scissor.c

--- stone_paper_Scissor.c
+++ stone_paper_Scissor.c
@@ -3,13 +3,13 @@ int main()
 {
   char input1,input2;
   scanf("%c %c",&input1,&input2);
-  if(input1 == 'p' and input2 == 's')
+  if(((input1 == 's') && (input2 == 'p')) || ((input1 == 'p') && (input2 == 's')))
     printf("s");
-  else if((input1 == 's') && (input2 == 'p') ||(input1 == 'p' and input2 == 's'))
-    printf("s");
-  else if(input1 == 'r' and input2 == 's') || (input1 == 's' and input2 == 'r'))
+  else if(((input1 == 'r') && (input2 == 's')) || ((input1 == 's') && (input2 == 'r')))
     printf("r");
-  else if((input1 == 'p') && (input2 == 'r') || (input1 == 'r' and input2 == 'p'))
+  else if(((input1 == 'p') && (input2 == 'r')) || ((input1 == 'r') && (input2 == 'p')))
+    printf("p");
+   else if(((input1 == 'r') && (input2 == 'p')) || ((input1 == 'p' && input2 == 'r')))
     printf("p");
   else 
     printf("d");
