d21bc72d05dfa9ecfbf2e9557603419879d9be88
Aishvarya-Varadharajan <49087166+Aishvarya-Varadharajan@users.noreply.github.com>
2019-06-10 16:38:53 +0530

    Create remove_vowel_reverse.c

---
+++ remove_vowel_reverse.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+  char str1[20],str2[20],str3[20];
+  int length,i,j=0;
+  scanf("%s",str1);
+  length=strlen(str1);
+  for(i=0;i<length;i++)
+  {
+    if((str[i] != 'a') && (str[i] != 'e') && (str[i] != 'i') && (str[i] != 'o') && (str[i] != 'a'))
+    {
+      str2[j]=str[i];
+      j++;
+    }
+   }
+   length=str2;
+   for(i=0;i<length;i++)
+   {
+      for(j=length;j<=0;j++)
+      {
+          str3[i] = str3[j];
+       }
+    }
+    printf("%s",str3);
+    return 0;
+  }
+   
+  
+   
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
